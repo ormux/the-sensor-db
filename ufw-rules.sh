7896289d22cd15168efa299902b94667c0d7a124
ormux <ormuseldesu@gmail.com>
2022-05-20 23:03:32 +1000

    updated rules

--- ufw-rules.sh
+++ ufw-rules.sh
@@ -2,14 +2,19 @@ config="/etc/ufw/before.rules"
 
 rule="-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT"
 
-ip=192.168.0.101
+# change this ip to that of the sysadmin
+admin=192.168.0.101
+
+# change this ip to that of the sql shield
+proxy=192.168.0.104
 
 case $1 in
    on)
      sudo sed -e "/$rule/ s/^/#/" -i $config
      sudo ufw disable
      sudo ufw default deny incoming
-     sudo ufw allow from $ip to any
+     sudo ufw allow from $admin to any
+     sudo ufw allow from $proxy to any
      sudo ufw enable
      sudo ufw reload
    ;;
@@ -17,7 +22,8 @@ case $1 in
      sudo sed -e "/$rule/ s/^#//" -i $config
      sudo ufw disable
      sudo ufw default allow incoming
-     sudo ufw delete allow from $ip to any
+     sudo ufw delete allow from $admin to any
+     sudo ufw delete allow from $proxy to any
      sudo ufw enable
      sudo ufw reload
    ;;
