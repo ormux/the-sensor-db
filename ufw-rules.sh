6c9e8f7efdc463e0e548fa6d10f42613c34bcf29
ormux <ormuseldesu@gmail.com>
2022-05-20 22:57:24 +1000

    add inline documentation

--- ufw-rules.sh
+++ ufw-rules.sh
@@ -2,12 +2,14 @@ config="/etc/ufw/before.rules"
 
 rule="-A ufw-before-input -p icmp --icmp-type echo-request -j ACCEPT"
 
+ip=192.168.0.101
+
 case $1 in
    on)
      sudo sed -e "/$rule/ s/^/#/" -i $config
      sudo ufw disable
      sudo ufw default deny incoming
-     sudo ufw allow from 192.168.0.101 to any
+     sudo ufw allow from $ip to any
      sudo ufw enable
      sudo ufw reload
    ;;
@@ -15,7 +17,7 @@ case $1 in
      sudo sed -e "/$rule/ s/^#//" -i $config
      sudo ufw disable
      sudo ufw default allow incoming
-     sudo ufw delete allow from 192.168.0.101 to any
+     sudo ufw delete allow from $ip to any
      sudo ufw enable
      sudo ufw reload
    ;;
